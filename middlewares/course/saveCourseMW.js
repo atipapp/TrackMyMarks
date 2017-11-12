d4bfbe3afad8f8d41449fc5fd8382f3c299992a7
pppttl <papp.attila971@gmail.com>
2017-11-12 13:47:24 +0100

    saveCourseMW implementation

--- middlewares/course/saveCourseMW.js
+++ middlewares/course/saveCourseMW.js
@@ -11,7 +11,35 @@ module.exports = function (objectrepository) {
 
     return function (req, res, next) {
         console.log('saveCourseMW');
-        return next();
+
+
+        if ((typeof req.body.name === 'undefined') ||
+            (typeof req.body.website === 'undefined')) {
+            return next();
+        }
+
+        var course = undefined;
+
+        if (typeof res.tpl.course !== 'undefined') {
+            course = res.tpl.course;
+        } else {
+            course = new courseModel();
+        }
+
+        course.name = req.body.name;
+        course.website = req.body.website;
+
+        console.log("\tNév:" + req.body.name); //DEBUG
+        console.log("\tWeb:" +req.body.website); //DEBUG
+
+        course.save(function (err, result) {
+            if (err) {
+                return next(err);
+            }
+
+            //return res.redirect('/courses/' + result.id);
+            return next();
+        });
     };
 
 };
