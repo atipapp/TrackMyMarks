df0602c8cb2a1d62b0d324b3f27f36dab1a5c13a
pppttl <papp.attila971@gmail.com>
2017-12-10 11:57:53 +0100

    mainRedirectMW test

---
+++ test/unit/generic/mainRedirectMW.js
@@ -0,0 +1,46 @@
+var expect = require('chai').expect;
+var mainRedirectMW = require('../../../middlewares/generic/mainRedirectMW');
+
+describe('mainRedirectMW', function () {
+    describe('User logged in', function () {
+        it('should call res.redirect to /courses if the userid in session exists', function (done) {
+
+
+            var reqMock = {
+                session: {
+                    userid: '5'
+                }
+            };
+            var resMock = {
+                redirect: function (newUrl) {
+                    expect(newUrl).be.eql('/courses');
+                    done();
+                }
+            };
+            mainRedirectMW({})(reqMock, resMock, function () {
+            });
+
+
+        });
+    });
+
+    describe('User not logged in', function () {
+        it('should call res.redirect to /login if the userid in session exists', function (done) {
+            var reqMock = {
+                session: {
+                }
+            };
+            var resMock = {
+                redirect: function (newUrl) {
+                    expect(newUrl).be.eql('/login');
+                    done();
+                }
+            };
+            mainRedirectMW({})(reqMock, resMock, function () {
+
+            });
+
+
+        });
+    });
+});
